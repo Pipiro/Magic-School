3c1b019b6d6dbe5dd03b3a41fd750c8d2736952d
Pipiro <therm.pierre@gmail.com>
2016-01-12 19:07:02 +0100

    Correction bug chemin

--- Source/classes/manager/ManagerFactory.class.php
+++ Source/classes/manager/ManagerFactory.class.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'].'ProjetLOL/Source/classes/manager/ApiKeyManager.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'Magic-School/Source/classes/manager/Player.class.php';
 
-require_once $_SERVER['DOCUMENT_ROOT'].'ProjetLOL/Source/classes/manager/pdo/PdoApiKeyManager.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'Magic-School/Source/classes/manager/pdo/PdoPlayerManager.class.php';
 
 ?>
